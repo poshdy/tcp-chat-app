079c6a8f5f16b2dd7cb3a0f6604098257d8e5b7c
Roshdy <126667610+poshdy@users.noreply.github.com>
2024-06-18 11:19:04 -0700

    add: readline module to read and write to console

--- chat-app/client.js
+++ chat-app/client.js
@@ -1,10 +1,25 @@
 const net = require("node:net");
-const config = require("../config");
-const client = net.createConnection({ port: config.port, host: config.host });
+const readline = require("node:readline/promises");
 
-client.on("connect", () => {
-  console.log("connected to server");
+const config = require("../config");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-client.on("end", () => console.log("Connection was ended!"));
-client.on("close", () => console.log("Connection was closed!"));
+const client = net.createConnection({ port: config.port, host: config.host });
+
+client.on("connect", async () => {
+  console.log("connected to server");
+  const message = await rl.question("enter a message:");
+  client.write(message);
+});
+
+client.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
+
+client.on("end", () => {
+  console.log("Connection was ended!");
+});
--- chat-app/server.js
+++ chat-app/server.js
@@ -1,17 +1,15 @@
 const net = require("node:net");
 const config = require("../config");
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const client = socket.address().port;
-    console.log(`client: ${client} says ${data.toString("utf-8")}`);
+    socket.write(data.toString("utf-8"));
   });
 });
 
 server.on("connection", (socket) => {
-  const clientDetails = socket.address();
-  console.log(`client: ${clientDetails.port} is connected`);
+  const clientDetails = socket.remotePort;
+  console.log(`client: ${clientDetails} is connected`);
 });
 
-server.listen(config.port, config.host, () => {
-  console.log(`TCP server is Running on ${host}:${port}`);
-});
+server.listen(config.port, config.host);
