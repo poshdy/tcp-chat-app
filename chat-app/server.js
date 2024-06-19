0e8aa4720c3dbdf2dc47a568f49bf84d7bfea7f5
Roshdy <126667610+poshdy@users.noreply.github.com>
2024-06-19 05:40:30 -0700

    feat: notifying users when someone joins or leaves chat

--- chat-app/server.js
+++ chat-app/server.js
@@ -8,6 +8,11 @@ server.on("connection", (socket) => {
   console.log(`A new connection to server`);
 
   let clientId = clients.length + 1;
+
+  clients.map((client) => {
+    client.socket.write(`User ${clientId} Joined!`);
+  });
+
   socket.write(`id-${clientId}`);
 
   socket.on("data", (data) => {
@@ -21,10 +26,20 @@ server.on("connection", (socket) => {
   });
 
   clients.push({ id: clientId.toString(), socket });
+  handleClientLeave(socket, clientId);
+  console.log(clients.map((cli) => cli.id));
 });
 
 server.listen(config.port, config.host);
 
-server.on("close", () => {
+server.on("end", () => {
   clients = [];
 });
+
+function handleClientLeave(socket, clientId) {
+  socket.on("error", () => {
+    clients.map((client) => {
+      client.socket.write(`User ${clientId} Left!`);
+    });
+  });
+}
